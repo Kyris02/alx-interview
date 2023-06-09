165f39095b5bfff271bf84806de2e4d87f228e2b
Tii04 <tiisetso.qsebata@gmail.com>
2023-06-09 06:43:29 +0200

    Implementing more understood algorithm

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,19 +4,18 @@
 
 
 def rotate_2d_matrix(matrix):
-    """ This function takes the matrix as an argument
-    and rotates it clowise."""
-
-    lengthOfArray = len(matrix)
-
-    # Transpose the matrix (push box from left to right)
-    for i in range(lengthOfArray):
-        for j in range(i + 1, lengthOfArray):
+    # transpose matrix (push box (matrix) over from left to right)
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Reverse each row (last row first, first row last)
-    for i in range(lengthOfArray):
-        matrix[i] = matrix[i][::-1]
+    # reverse matrix (first row becomes last, last row becomes first)
+    lengthOfArray = len(matrix)
+    for i in range(lengthOfArray // 2):
+        for j in range(lengthOfArray):
+            matrix[j][i], matrix[j][lengthOfArray - 1 - i] = (
+                matrix[j][lengthOfArray - 1 - i], matrix[j][i]
+            )
 
 
 if __name__ == "__main__":
